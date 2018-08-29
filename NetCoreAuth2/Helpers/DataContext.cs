1bf069c1db010c8ebb83a88663f4d4e562803bee
Sean You <seon2you@hotmail.com>
2018-08-29 00:31:55 -0700

    added models and dbcontext

---
+++ NetCoreAuth2/Helpers/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NetCoreAuth2.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreAuth2.Helpers
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+        public DbSet<User> Users { get; set; }
+    }
+}
