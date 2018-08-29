f8e7c172cd3f6e8f60b490ef1eefa7d0df4c9b6b
Sean You <seon2you@hotmail.com>
2018-08-29 04:25:33 -0700

    added users controller

---
+++ NetCoreAuth2/Helpers/AutoMapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using NetCoreAuth2.Dtos;
+using NetCoreAuth2.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreAuth2.Helpers
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<User, UserDto>();
+            CreateMap<UserDto, User>();
+        }        
+    }
+}
